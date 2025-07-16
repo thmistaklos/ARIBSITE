776742a6459448d635240b832c7bb974a690ff8c
[dyad] <git@dyad.sh>
2025-07-16 20:50:39 +0100

    [dyad] Updated PublicLayout with all public routes and footer - wrote 1 file(s)

--- src/components/PublicLayout.tsx
+++ src/components/PublicLayout.tsx
@@ -3,9 +3,15 @@ import { useLocation, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Header from "./Header";
 import HomePage from "../pages/HomePage";
-// ✅ Import other pages as needed
-// import AboutPage from "../pages/AboutPage";
-// import ContactPage from "../pages/ContactPage";
+import AboutPage from "../pages/AboutPage";
+import ContactPage from "../pages/ContactPage";
+import DistributorsPage from "../pages/DistributorsPage";
+import RecipesPage from "../pages/RecipesPage";
+import BlogPage from "../pages/BlogPage";
+import BlogPostDetail from "../pages/BlogPostDetail";
+import ProductsPage from "../pages/ProductsPage";
+import ProductDetailPage from "../pages/ProductDetailPage";
+import Footer from "./Footer";
 
 const PublicLayout: React.FC = () => {
   const location = useLocation();
@@ -17,12 +23,19 @@ const PublicLayout: React.FC = () => {
         <AnimatePresence mode="wait">
           <Routes location={location} key={location.pathname}>
             <Route path="/" element={<HomePage />} />
-            {/* ✅ Add your other routes here */}
-            {/* <Route path="/about" element={<AboutPage />} /> */}
-            {/* <Route path="/contact" element={<ContactPage />} /> */}
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/distributors" element={<DistributorsPage />} />
+            <Route path="/recipes" element={<RecipesPage />} />
+            <Route path="/recipes/:id" element={<RecipeDetailPage />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog/:id" element={<BlogPostDetail />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/:id" element={<ProductDetailPage />} />
           </Routes>
         </AnimatePresence>
       </main>
+      <Footer />
     </>
   );
 };
