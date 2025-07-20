d2aeff3e16cff934e8ac2f7cce58c74aef0e4f74
[dyad] <ahmedbelaribi24@gmail.com>
2025-07-20 17:04:36 +0100

    [dyad] wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,9 +38,15 @@ const Header: React.FC = () => {
       <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
         {/* Logo/Brand Name */}
         <Link to="/" className="flex items-center space-x-2">
-          <img src="https://goykvqomwqwqklyizeed.supabase.co/storage/v1/object/public/logosandstuff//ChatGPT_Image_Jul_17__2025__10_17_07_PM-removebg-preview.png" alt="ARIB DAIRY Logo" className="h-8 w-8 object-contain" />
           <span className="text-xl font-bold text-dairy-darkBlue">{t('arib_dairy')}</span>
-          <img src="https://goykvqomwqwqklyizeed.supabase.co/storage/v1/object/public/logosandstuff//0222-removebg-preview%20(1).png" alt="ARIB DAIRY Second Logo" className="h-8 w-8 object-contain" />
+          <motion.div
+            initial={{ rotate: 0 }}
+            animate={{ rotate: [0, -10, 10, -10, 0] }}
+            transition={{ repeat: Infinity, duration: 5, ease: 'easeInOut' }}
+            className="flex justify-center"
+          >
+            <img src="https://goykvqomwqwqklyizeed.supabase.co/storage/v1/object/public/logosandstuff//ChatGPT_Image_Jul_17__2025__10_17_07_PM-removebg-preview.png" alt="ARIB DAIRY Logo" className="h-8 w-8 object-contain" />
+          </motion.div>
         </Link>
 
         {isMobile ? (
