b16024434272281f3df6105f5c11b9512ad0911f
[dyad] <git@dyad.sh>
2025-07-22 23:31:39 +0100

    [dyad] Removed all popup components - wrote 1 file(s), deleted 3 file(s)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,8 +9,6 @@ import RecipesSection from '@/components/RecipesSection';
 import FactsSection from '@/components/FactsSection';
 import AccordionSection from '@/components/AccordionSection';
 import ParticleBackground from '@/components/ParticleBackground';
-import FloatingFlyer from '@/components/FloatingFlyer';
-import DiscountPopup from '@/components/DiscountPopup';
 import HeroCarousel from '@/components/HeroCarousel';
 import ImageSliderSection from '@/components/ImageSliderSection';
 // Removed import for BannerSection
@@ -55,12 +53,6 @@ const HomePage: React.FC = () => {
       <section className="relative z-20 overflow-hidden pb-8 pt-10 lg:pb-[90px] lg:pt-[120px] bg-dairy-cream text-dairy-text">
         <AccordionSection />
       </section>
-
-      {/* Floating Flyer */}
-      <FloatingFlyer />
-
-      {/* Discount Popup */}
-      <DiscountPopup />
     </div>
   );
 };
